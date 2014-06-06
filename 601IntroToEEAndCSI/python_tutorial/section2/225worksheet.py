fb0d527b15e98372366ff4edd970d11ce86cc881
Alex Villa <avilla0429@gmail.com>
2014-06-06 00:19:31 -0400

    Completed worksheet 2.2.5 for the Python Tutorial

---
+++ 601IntroToEEAndCSI/python_tutorial/section2/225worksheet.py
@@ -0,0 +1,20 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../modules/'))
+
+import checkers
+import math
+
+#This is just asking for me to create a function that calculates distance!
+
+def evalQuadratic(a, b, c, x):
+  return a*x**2 + b*x + c
+
+question = evalQuadratic(1, 1, 1, 1)
+checkers.check_answer(question, 3)
+
+question = evalQuadratic(2, 2, 2, 2)
+checkers.check_answer(question, 14)
+
+question = evalQuadratic(2, 3, 4, 5)
+checkers.check_answer(question, 69) #hehe
