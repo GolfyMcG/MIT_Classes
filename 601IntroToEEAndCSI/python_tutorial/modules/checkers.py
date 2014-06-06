2e8f92310b2ecc63d3413a77073fbc29609c7853
Alex Villa <avilla0429@gmail.com>
2014-06-05 23:41:25 -0400

    Complete section 2 of Python Tutorial 2.1.1 Worksheet

--- 601IntroToEEAndCSI/python_tutorial/modules/checkers.py
+++ 601IntroToEEAndCSI/python_tutorial/modules/checkers.py
@@ -1,6 +1,13 @@
+import types
+
 def check_answer(question, answer):
   if question is type(None) or question is None:
     if answer == "NoneType" or answer == "None":
+      print "Correct"
+    else:
+      print "Wrong"
+  elif isinstance(question, types.FunctionType):
+    if answer == "Function":
       print "Correct"
     else:
       print "Wrong"
--- 601IntroToEEAndCSI/python_tutorial/section2/211worksheet.py
+++ 601IntroToEEAndCSI/python_tutorial/section2/211worksheet.py
@@ -3,8 +3,10 @@ import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../modules/'))
 
 import math
+import types
 import checkers
 
+#PART 1
 def a(x):
   return x + 1
 
@@ -36,3 +38,35 @@ def f(x, y):
   x + y - 2
 
 checkers.check_answer(type(f(1,2)), "NoneType")
+
+#PART 2
+question = a(6)
+checkers.check_answer(question, 7)
+checkers.check_answer(type(question), int)
+
+question = a(-5.3)
+checkers.check_answer(question, -4.3)
+checkers.check_answer(type(question), float)
+
+question = a(a(a(6)))
+checkers.check_answer(question, 9)
+checkers.check_answer(type(question), int)
+
+question = c(a(1),b(1))
+checkers.check_answer(question, 4.0)
+checkers.check_answer(type(question), float)
+
+question = d(10, 11.1)
+checkers.check_answer(question, False)
+checkers.check_answer(type(question), bool)
+
+question = e(a(3),b(4), c(3,4))
+# x = 4
+# y = 5.0
+# z = 7
+checkers.check_answer(question, False)
+checkers.check_answer(type(question), bool)
+
+question = e
+checkers.check_answer(question, "Function")
+checkers.check_answer(type(question), types.FunctionType)
