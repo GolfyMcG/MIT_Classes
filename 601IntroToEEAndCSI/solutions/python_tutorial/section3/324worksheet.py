2dade87b97502112b75f7dd96711a9ac9e5380e8
Alex Villa <avilla0429@gmail.com>
2014-06-10 01:30:38 -0400

    Fixes part of worksheet 323 and completes worksheet 324

--- 601IntroToEEAndCSI/solutions/python_tutorial/section3/324worksheet.py
+++ 601IntroToEEAndCSI/solutions/python_tutorial/section3/324worksheet.py
@@ -4,27 +4,29 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../modules/'))
 
 import checkers
 
-def mod(m, n):
+def div(m, n):
   if (isinstance(n, int) and n > 0) and (isinstance(m, int) and m > 0):
+    i = 0
     while m >= n:
       m = m - n
+      i = i + 1
 
-    return m
+    return i
   else:
     print "Both arguments must be positive integers"
 
 
-question = mod(3, 2)
+question = div(3, 2)
 checkers.check_answer(question, 1)
 
-question = mod(2, 3)
-checkers.check_answer(question, 2)
-
-question = mod(11, 7)
-checkers.check_answer(question, 4)
-
-question = mod(11, 2)
-checkers.check_answer(question, 1)
-
-question = mod(10, 2)
+question = div(2, 3)
 checkers.check_answer(question, 0)
+
+question = div(11, 7)
+checkers.check_answer(question, 1)
+
+question = div(11, 2)
+checkers.check_answer(question, 5)
+
+question = div(6, 2)
+checkers.check_answer(question, 3)
