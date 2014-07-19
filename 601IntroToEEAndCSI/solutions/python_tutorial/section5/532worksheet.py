b3479f2bef54cb6bb4559b5f896a9fe8ff5920b7
Alex Villa <avilla0429@gmail.com>
2014-07-19 01:49:56 -0400

    Finishes 532 worksheet

---
+++ 601IntroToEEAndCSI/solutions/python_tutorial/section5/532worksheet.py
@@ -0,0 +1,24 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../modules/'))
+
+import checkers
+import math
+
+def everyOther(l):
+  return [l[x] for x in range(0, len(l), 2)]
+
+n = 1
+question = everyOther([1, 2, 3, 4, 5])
+ansvalue = [1, 3, 5]
+checkers.check_answer(question, ansvalue, n)
+
+n += 1
+question = everyOther(['this', 'is', 'a', 'sentence'])
+ansvalue = ['this', 'a']
+checkers.check_answer(question, ansvalue, n)
+
+n += 1
+question = everyOther(['oh no', 'this', 'is', 'a', 'another', 'longer', 'sentence'])
+ansvalue = ['oh no', 'is', 'another', 'sentence']
+checkers.check_answer(question, ansvalue, n)
