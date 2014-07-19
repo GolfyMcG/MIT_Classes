e144f20b06fd391519dcd63bb726dd55de2cbd95
Alex Villa <avilla0429@gmail.com>
2014-07-19 01:37:09 -0400

    Finishes 522 worksheet

---
+++ 601IntroToEEAndCSI/solutions/python_tutorial/section5/522worksheet.py
@@ -0,0 +1,30 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../modules/'))
+
+import checkers
+import math
+
+def pointDist(p1, p2):
+  dx = p1[0] - p2[0]
+  dy = p1[1] - p2[1]
+  result = round(math.sqrt(dx**2 + dy**2), 3)
+  return result
+
+def perpDist(p, l):
+  (px, py) = p
+  (a, b, c) = l
+
+  cn = py - px*(b/a)
+  xs = (-(c*a)/(b**2) - cn*(a/b))/(1 + ((a/b)**2))
+  yo = (-a/b)*xs - (c/b)
+
+  ps = (xs, yo)
+  shrt_distance = round(pointDist(ps, p)*100)/100
+  print shrt_distance
+  return shrt_distance
+
+p = (10, 0)
+l = (1, 1, 1)
+question = perpDist(p, l)
+checkers.check_answer(question, 7.81)
