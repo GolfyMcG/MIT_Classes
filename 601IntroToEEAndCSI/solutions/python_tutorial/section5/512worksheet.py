3ff7a5f68d7c116d15c510a7fabbc4b66fbdcacb
Alex Villa <avilla0429@gmail.com>
2014-07-19 01:14:40 -0400

    Finishes 512worksheet

--- 601IntroToEEAndCSI/solutions/python_tutorial/section5/512worksheet.py
+++ 601IntroToEEAndCSI/solutions/python_tutorial/section5/512worksheet.py
@@ -4,9 +4,27 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../modules/'))
 
 import checkers
 
-n =
-question = a
-ansValue = 'hi'
-ansType = str
-checkers.check_answer(question, ansValue, n)
-checkers.check_answer(type(question), ansType, n)
+n = 1
+question = [0, 1, 2, 3, 4]
+ansvalue = range(0, 5)
+checkers.check_answer(question, ansvalue, n)
+
+n += 1
+question = [44, 45, 46]
+ansvalue = range(44, 47)
+checkers.check_answer(question, ansvalue, n)
+
+n += 1
+question = [-3, -2, -1, 0, 1]
+ansvalue = range(-3, 2)
+checkers.check_answer(question, ansvalue, n)
+
+n += 1
+question = [1, 3, 5, 7]
+ansvalue = range(1, 8, 2)
+checkers.check_answer(question, ansvalue, n)
+
+n += 1
+question = [5, 4, 3, 2]
+ansvalue = range(5, 1, -1)
+checkers.check_answer(question, ansvalue, n)
